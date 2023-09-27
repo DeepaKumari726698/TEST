30158112452ce1733fb48c7aba566afd2e6e479a
DeepaKumari726698 <134695560+DeepaKumari726698@users.noreply.github.com>
2023-09-27 17:29:15 +0530

    Add files via upload

---
+++ Home.js
@@ -0,0 +1,18 @@
+$(document).ready(function(){
+    $('input.success').addClass('error')
+})
+// document.write(" Hello WorldKHK <br>");
+// document.write("<i><b>Hellow!!</b></i> <br>")
+// var z = "<i>Welcome to 455 </i><br>";
+// z = 66;
+// var x = 'waho100.253454';
+// x = "y3883";
+// // z = '<i>wows66</i><br>';
+// document.write(z);
+// document.write(z);
+// document.write(x);
+// document.wrte(x);
+// $(Document).ready(function(){
+//     console.table(da)
+// })
+
