822223c4b37a32749a68b4d3e6d35b1746a61e88
DeepaKumari726698 <134695560+DeepaKumari726698@users.noreply.github.com>
2023-10-22 12:59:17 +0530

    Add files via upload

---
+++ data.js
@@ -0,0 +1,164 @@
+var data = {
+  "students": [
+      {
+          "id": 1,
+          "name": "Aarav Patel",
+          "age": 16,
+          "class": "10",
+          "address": "22, Lokhandwala Road, Mumbai",
+          "grade": "B"
+      },
+      {
+          "id": 2,
+          "name": "Diya Bhatt",
+          "age": 17,
+          "class": "11",
+          "address": "45, Sarojini Nagar, New Delhi",
+          "grade": "A"
+      },
+      {
+          "id": 3,
+          "name": "Ranvir Kapoor",
+          "age": 15,
+          "class": "9",
+          "address": "18, Park Street, Kolkata",
+          "grade": "B+"
+      },
+      {
+          "id": 4,
+          "name": "Riya Agrahni",
+          "age": 16,
+          "class": "10",
+          "address": "32, Anna Nagar, Chennai",
+          "grade": "A-"
+      },
+      {
+          "id": 5,
+          "name": "Aditya Reddy",
+          "age": 18,
+          "class": "12",
+          "address": "11, Jubilee Hills, Hyderabad",
+          "grade": "A+"
+      },
+      {
+          "id": 6,
+          "name": "Sanvi Sharma",
+          "age": 17,
+          "class": "11",
+          "address": "76, Whitefield, Bangalore",
+          "grade": "B"
+      },
+      {
+          "id": 7,
+          "name": "Mohit Kumar",
+          "age": 15,
+          "class": "9",
+          "address": "24, Shivaji Nagar, Pune",
+          "grade": "C+"
+      },
+      {
+          "id": 8,
+          "name": "Ananya Joshi",
+          "age": 16,
+          "class": "10",
+          "address": "39, Kalyan Nagar, Mumbai",
+          "grade": "A"
+      },
+      {
+          "id": 9,
+          "name": "Vishal Joshi",
+          "age": 18,
+          "class": "12",
+          "address": "88, Vasanth Nagar, Bangalore",
+          "grade": "B-"
+      },
+      {
+          "id": 10,
+          "name": "Abhay Desai",
+          "age": 16,
+          "class": "10",
+          "address": "5, Saket, New Delhi",
+          "grade": "A"
+      },
+      {
+          "id": 11,
+          "name": "Siddharth Nair",
+          "age": 15,
+          "class": "9",
+          "address": "42, BTM Layout, Bangalore",
+          "grade": "C"
+      },
+      {
+          "id": 12,
+          "name": "Kriti Malhotra",
+          "age": 17,
+          "class": "11",
+          "address": "12, Salt Lake, Kolkata",
+          "grade": "B"
+      },
+      {
+          "id": 13,
+          "name": "Hinashi jayshaval",
+          "age": 16,
+          "class": "10",
+          "address": "66, MG Road, Pune",
+          "grade": "B+"
+      },
+      {
+          "id": 14,
+          "name": "Neha Agrwal",
+          "age": 18,
+          "class": "12",
+          "address": "90, DLF Gurgaon, Delhi",
+          "grade": "A-"
+      },
+      {
+          "id": 15,
+          "name": "Rohan Mehra",
+          "age": 15,
+          "class": "9",
+          "address": "31, Besant Nagar, Chennai",
+          "grade": "B"
+      },
+      {
+          "id": 16,
+          "name": "Juhi Gupta",
+          "age": 17,
+          "class": "11",
+          "address": "19, Hauz Khas, New Delhi",
+          "grade": "A"
+      },
+      {
+          "id": 17,
+          "name": "Aman Yadav",
+          "age": 16,
+          "class": "10",
+          "address": "55, Koramangala, Bangalore",
+          "grade": "A+"
+      },
+      {
+          "id": 18,
+          "name": "Roshani Pathak",
+          "age": 15,
+          "class": "9",
+          "address": "2, Model Town, Ludhiana",
+          "grade": "B-"
+      },
+      {
+          "id": 19,
+          "name": "Monika Pathak",
+          "age": 17,
+          "class": "11",
+          "address": "23, Lajpat Nagar, Delhi",
+          "grade": "A-"
+      },
+      {
+          "id": 20,
+          "name": "Divya Shahni",
+          "age": 18,
+          "class": "12",
+          "address": "77, Vashi, Mumbai",
+          "grade": "B+"
+      }
+  ]
+}
